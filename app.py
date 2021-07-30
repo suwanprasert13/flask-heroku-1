3c0ac9884e77d3f61903b125b2bd3941bbc68cc2
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-07-30 09:54:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,9 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/name')
+def name():
+    return "อาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/10"
+
 if __name__ == "__main__":
     app.run(debug=False)
