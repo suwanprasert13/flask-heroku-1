6d7b0a08b39883d67bb3fd45c0faa9ee84e7717c
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-08-30 13:52:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,6 @@ from flask import Flask, jsonify
 from flask import Flask, render_template, Response
 import cv2
 
-
-data = [
-        {
-            "id": 1,
-            "library": "Pandas",
-            "language": "Python"
-        },
-        {
-            "id": 2,
-            "library": "requests",
-            "language": "Python"
-        },
-        {
-            "id": 3,
-            "library": "NumPy",
-            "language": "Python"
-        }
-    ]
-
 @app.route('/')
 def hello():
     return "Hello Flask-Heroku"
