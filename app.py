1103c0e879c0bd87bc8178e552f7235c01d4cf80
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-08-31 09:17:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,31 @@ import cv2
 
 app = Flask(__name__)
 
+data = [
+        {
+            "id": 1,
+            "library": "Pandas",
+            "language": "Python"
+        },
+        {
+            "id": 2,
+            "library": "requests",
+            "language": "Python"
+        },
+        {
+            "id": 3,
+            "library": "NumPy",
+            "language": "Python"
+        }
+    ]
+
 @app.route('/')
 def hello():
-    return "Hello Flask-Heroku"
+    return "Hello Flask-Herok"
 
-@app.route('/test')
-def test():
-    return "สวัสดี"
+@app.route('/api', methods=['GET'])
+def get_api():
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
