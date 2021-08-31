0ae063cd5f3ad3d6bdf46457fc7e2eb819858275
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-08-31 10:14:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_api():
 @app.route('/hi')
 @app.route('/hello')
 def Home():
-        return "<h1>Hello World!</h1>"
+    return "<h1>Hello World!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
