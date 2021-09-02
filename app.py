2924e41dc18e52953dc8fc2ea7c49531c423f7e1
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-09-02 10:47:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-heroku"
+    return "Hello อาคม สุวรรรประเสริฐ เลขที่ 0 ชั้น ม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
