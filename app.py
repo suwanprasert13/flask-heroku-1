d037dcdf5fb7fe9d9b2f8c0c6fa7583635c44749
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-09-03 11:39:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello อาคม สุวรรรประเสริฐ เลขที่ 0 ชั้น ม.4/6"
+    return "Hello อาคม สุวรรรประเสริฐ เลขที่ 0 ชั้น ม.4/7"
 
 @app.route('/api', methods=['GET'])
 def get_api():
